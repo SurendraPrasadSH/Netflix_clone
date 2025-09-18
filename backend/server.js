8944a24a29f0c02cdb4bae1afc76cd20eef2cd0f
Surendra Prasad <surendrap792@gmail.com>
2025-09-19 05:27:30 +0530

    adding few lines of code to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
+app.use(cors());  
 app.use(bodyParser.json());
 
 const supabaseUrl = 'https://vjidfgmqbdlkknpxozvf.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqaWRmZ21xYmRsa2tucHhvenZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMzA2NDQsImV4cCI6MjA3MzgwNjY0NH0.gMDjBn2x70X5rZ0OJpvZ8tObdJBqLiIy4oJxXPqUjes';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const cors = require('cors');
-app.use(cors());
 
 app.post('/signup', async (req, res) => {
   const { email } = req.body;
